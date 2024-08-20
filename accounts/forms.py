31b020cb582d2d4261034e8c3c10525f21e3258b
Seungju_Yi <yi77371463@gmail.com>
2024-08-20 10:52:23 +0900

    feat: 새 프로잭트 생성
    1. django 설치==4.2
    2. app생성 및 등록 (accounts, products, users)
    3. app url경로 지정
    4. base template 경로 지정 및 생성
    5. accounts.models.User 모델 생성 및 기본 유저 모델 등록
    6. accounts.form생성
       -회원가입 폼 커스텀: 커스텀usermodel 상속
       -회원정보수정 폼 커스텀: 비밀번호수정 href문자열 수정,
       fields(성, 이름, 이메일, 아이디)
    7. accounts 기능 구현
       -회원가입
       -로그인, 로그아웃
       -회원정보수정, 비밀번호수정
       -회원탈퇴

---
+++ accounts/forms.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, UsernameField
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = UserCreationForm.Meta.fields + ()
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = ("first_name", "last_name", "email", "username")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        if self.fields.get("password"):
+            password_help_text = ('<a href="{}">비밀번호변경</a>.').format(f"{reverse('accounts:update_password')}")
+            self.fields['password'].help_text = password_help_text
---
+++ accounts/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import AuthenticationForm,  PasswordChangeForm
+from django.views.decorators.http import require_http_methods, require_POST
+
+
+@require_http_methods(["GET", "POST"])
+def login(request):
+    if request.method == "POST": #내가 데이터를 치고 들어온것
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+        return redirect('products:products')
+    else: #url 내가 직접 치고온거 or <a href=''></a>
+        form = AuthenticationForm()
+    context = { "form": form }  
+    return render(request, 'accounts/login.html', context)
+
+
+@require_POST
+def logout(request):
+    auth_logout(request)
+    return redirect('products:products')
+
+
+@require_http_methods(["GET", "POST"])
+def signup(request):
+    if request.method == "POST":   #내가 데이터를 치고 들어온것
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():     #폼 유효성 검사
+            user = form.save()
+            auth_login(request, user)   #로그인, 세션만들기V
+        return redirect('products:products')
+    else:     #url 내가 직접 치고온거 or <a href=''></a>
+        form = CustomUserCreationForm()   #데이터를 입력하는창
+    context = {'form': form }
+    return render(request, 'accounts/signup.html', context)
+
+
+@require_http_methods(["GET","POST"])
+def update(request):
+    if request.method == "POST":
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("products:products")
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {"form": form}
+    return render(request, 'accounts/update.html', context)
+
+
+@require_http_methods(["GET","POST"])
+def update_password(request):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user) 
+            return redirect('products:products')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {'form': form}
+    return render(request,'accounts/update_password.html', context)
+
+
+@require_POST
+def delete(request):
+    print(request.user)
+    if request.user.is_authenticated:
+        request.user.delete()
+        auth_logout(request)
+    return redirect('products:products')
