4a8a9dba2921c2de3205c7641bc9595dbdb35d64
Seungju_Yi <yi77371463@gmail.com>
2024-08-21 10:47:39 +0900

    임시

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from accounts.models import User
+from django.contrib.auth import get_user_model
+
 
 # Create your views here.
+def profile(request, username):
+    member = get_object_or_404(get_user_model(), username=username)
+    context = { 'member' : member }
+    return render(request, 'users/profile.html', context)
