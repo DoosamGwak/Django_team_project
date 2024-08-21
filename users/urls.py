f6df94463bbae14cf4888c3b5b93fccadf8e6ec0
strongdambi <thekeytohong@gmail.com>
2024-08-21 19:41:05 +0900

    feat: user app basic setting
    - url: users, profile 추가
    - view: usersm profile 기본 코드 추가
    - templates 폴더 생성 후 users, profile 각각 html 파일 생성

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 
-app_name = ''
+app_name = 'users'
 urlpatterns = [
+    path("", views.users, name="users"),
+    path("profile/<str:username>/", views.profile, name="profile"),
 ]
